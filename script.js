211143d787ffe4b1a30a02ea332d24604c9fd649
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2026-03-10 01:39:48 +0600

    script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello, World! This is a simple JavaScript file.");
 const loadNavbar = () => {
   const navbarHTML = `
-    <nav class="navbar bg-green-400 shadow-sm">
+    <nav class="navbar bg-red-50 shadow-sm">
       <div class="navbar md:w-4/5 md:mx-auto">
         <div class="navbar-start">
           <div class="dropdown">
@@ -64,4 +64,135 @@ const loadNavbar = () => {
   }
 };
 
+const loadFooter = () => {
+  const footerHTML = `
+    <footer class="footer sm:footer-horizontal bg-red-50 text-base-content p-10">
+  <nav>
+    <h6 class="footer-title">Services</h6>
+    <a class="link link-hover">Branding</a>
+    <a class="link link-hover">Cloths</a>
+    <a class="link link-hover">Devices</a>
+    <a class="link link-hover">Accessories</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Company</h6>
+    <a class="link link-hover">About us</a>
+    <a class="link link-hover">Contact</a>
+    <a class="link link-hover">Products</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Legal</h6>
+    <a class="link link-hover">Terms of use</a>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </nav>
+  <form>
+    <h6 class="footer-title">Newsletter</h6>
+    <fieldset class="w-80">
+      <label>Enter your email address</label>
+      <div class="join">
+        <input
+          type="text"
+          placeholder="username@site.com"
+          class="input input-bordered join-item" />
+        <button class="btn btn-primary join-item">Subscribe</button>
+      </div>
+    </fieldset>
+  </form>
+</footer>
+  `;
+
+  const container = document.getElementById("footer-container");
+  if (container) {
+    container.innerHTML = footerHTML;
+  }
+};
+
+// load trending products based on the rating and with the help of the API
+const loadProducts = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      const trendingProducts = data
+        .filter((product) => product.rating.rate >= 4.0)
+        .slice(0, 3);
+      const container = document.getElementById("trending-product");
+      const allProducts = document.getElementById("all-products");
+      const categories = [...new Set(data.map((product) => product.category))];
+      if (container) {
+        let productsHTML = `
+              <h2 class="text-3xl font-bold text-center mb-10">Trending Products</h2>
+              <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
+                ${trendingProducts
+                  .map(
+                    (product) => `
+                  <div class="card bg-base-100 shadow-xl">
+                    <figure>
+                      <img class="h-48 object-contain" src="${product.image}" alt="${product.title}" />
+                    </figure>
+                    <div class="card-body bg-red-50">
+                      <div class="flex justify-between mb-2">
+                      <div class="btn btn-xs btn-secondary rounded-full font-semibold uppercase">${product.category}</div>
+                      <div class="rounded-full font-semibold uppercase"><i class="fa-solid fa-star text-amber-400"></i>${product.rating.rate}(${product.rating.count})</div>
+                      </div>
+                      <h2 class="card-title">${product.title}</h2>
+                      <p>${product.description.slice(0, 100)}...</p>
+                      <p class="text-xl font-bold">$${product.price.toFixed(2)}</p>
+                      <div class="card-actions flex justify-between ">
+                        <button class="flex-1 btn btn-outline btn-sm">Details <i class="fa-solid fa-info-circle"></i></button>
+                        <button class="flex-1 btn btn-primary btn-sm">Cart <i class="fa-solid fa-shopping-cart"></i></button>
+                      </div>
+                    </div>
+                  </div>
+                `,
+                  )
+                  .join("")}
+              </div>
+            `;
+        container.innerHTML = productsHTML;
+      } else if (allProducts) {
+        let productsHTML = `
+              <h2 class="text-3xl font-bold text-center mb-10">Our Products</h2>
+             <div class="flex flex-wrap gap-4 justify-center mb-10">
+              ${categories
+                .map(
+                  (category) => `
+                    <h3 class="text-md font-semibold btn btn-outline btn-secondary btn-xs rounded-full uppercase">${category}</h3>
+                  `,
+                )
+                .join("")}
+             </div>
+              <div class="grid grid-cols-1 md:grid-cols-3 gap-10">
+                ${data
+                  .map(
+                    (product) => `
+                  <div class="card bg-base-100 shadow-xl">
+                    <figure>
+                      <img class="h-48 object-contain" src="${product.image}" alt="${product.title}" />
+                    </figure>
+                    <div class="card-body bg-red-50">
+                      <div class="flex justify-between mb-2">
+                      <div class="btn btn-xs btn-secondary rounded-full font-semibold uppercase">${product.category}</div>
+                      <div class="rounded-full font-semibold uppercase"><i class="fa-solid fa-star text-amber-400"></i>${product.rating.rate}(${product.rating.count})</div>
+                      </div>
+                      <h2 class="card-title">${product.title}</h2>
+                      <p>${product.description.slice(0, 100)}...</p>
+                      <p class="text-xl font-bold">$${product.price.toFixed(2)}</p>
+                      <div class="card-actions flex justify-between ">
+                        <button class="flex-1 btn btn-outline btn-sm">Details <i class="fa-solid fa-info-circle"></i></button>
+                        <button class="flex-1 btn btn-primary btn-sm">Cart <i class="fa-solid fa-shopping-cart"></i></button>
+                      </div>
+                    </div>
+                  </div>
+                `,
+                  )
+                  .join("")}
+              </div>
+            `;
+        allProducts.innerHTML = productsHTML;
+      }
+    });
+};
 loadNavbar();
+loadProducts();
+loadFooter();
